b6998810762ff34e04186ebfc965d28e716e4a3b
wuyt <594228924@qq.com>
2016-12-23 00:56:25 +0800

    加了弹出视图

---
+++ PtpurExtensionDemo/AMAInstructionsCell.swift
@@ -0,0 +1,18 @@
+//
+//  AMAInstructionsCell.swift
+//  PtpurExtensionDemo
+//
+//  Created by 吴玉铁 on 2016/12/22.
+//  Copyright © 2016年 wuyutie. All rights reserved.
+//
+
+import UIKit
+
+class AMAInstructionsCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ PtpurExtensionDemo/AMAInstructionsView.swift
@@ -0,0 +1,21 @@
+//
+//  AMAInstructionsView.swift
+//  PtpurExtensionDemo
+//
+//  Created by 吴玉铁 on 2016/12/22.
+//  Copyright © 2016年 wuyutie. All rights reserved.
+//
+
+import UIKit
+
+class AMAInstructionsView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
