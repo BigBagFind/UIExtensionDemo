ce1461206a820253cdaec76e1d7b84ec8fd358e5
TieGege <wuyutie2008@163.com>
2017-06-27 00:24:19 +0800

    push

--- PtpurExtensionDemo/HoverMainViewController.m
+++ PtpurExtensionDemo/HoverMainViewController.m
@@ -9,13 +9,13 @@
 #import "HoverMainViewController.h"
 #import "HXVTableViewController.h"
 
-@interface HoverMainViewController () <HXVTableViewScrollDelegate>
+@interface HoverMainViewController () <HXVTableViewScrollDelegate, UIScrollViewDelegate>
 
 @property (nonatomic, strong) UIScrollView *scrollView;
 
-@property (nonatomic, strong) UIView *headerView;
+@property (nonatomic, strong) UILabel *headerView;
 
-@property (nonatomic, strong) UIView *hoverView;
+@property (nonatomic, strong) UILabel *hoverView;
 
 
 @end
@@ -26,19 +26,26 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor greenColor];
     
+    [self.navigationController.navigationBar setBackgroundImage:[[UIImage alloc]init] forBarMetrics:UIBarMetricsDefault];
+    
+    [self.navigationController.navigationBar setShadowImage:[[UIImage alloc] init]];
+    
     _scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 64, self.view.bounds.size.width, self.view.frame.size.height - 64)];
     [self.view addSubview:_scrollView];
     _scrollView.backgroundColor = [UIColor orangeColor];
     _scrollView.pagingEnabled = YES;
     _scrollView.contentSize = CGSizeMake(self.view.bounds.size.width * 3, 0);
+    _scrollView.delegate = self;
     
-    _headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, 200)];
+    _headerView = [[UILabel alloc] initWithFrame:CGRectMake(0, 64, self.view.bounds.size.width, 200)];
     _headerView.backgroundColor = [UIColor yellowColor];
+    _headerView.text = @"HeaderView";
     [self.view addSubview:_headerView];
     
-    _hoverView = [[UIView alloc] initWithFrame:CGRectMake(0, 200, self.view.bounds.size.width, 50)];
+    _hoverView = [[UILabel alloc] initWithFrame:CGRectMake(0, 264, self.view.bounds.size.width, 50)];
     _hoverView.backgroundColor = [UIColor cyanColor];
     [self.view addSubview:_hoverView];
+    _hoverView.text = @"HoverView";
     
     self.automaticallyAdjustsScrollViewInsets = NO;
     [self addSubControllers];
@@ -68,6 +75,7 @@
         
         [VC setHeaderViewYOffset:250];
         
+        [VC fillFooterViewWithContentHeight:44 * 10];
     }
 }
 
@@ -76,39 +84,57 @@
 #pragma mark - HXVScrollDelegate
 - (void)hxv_tableViewDidScroll:(UIScrollView *)scrollView {
     CGFloat offsetY = scrollView.contentOffset.y;
-    if (offsetY <= 0) {
-        CGRect rect = _headerView.frame;
-        rect.origin.y = 0;
-        _headerView.frame = rect;
+    NSLog(@"%lf", offsetY);
+    
+    CGRect rect = _headerView.frame;
+    rect.origin.y = MAX(-offsetY + 64, -200 + 64);
+    _headerView.frame = rect;
+    
+    CGFloat originY = 64;
+    CGRect hoverRect = _hoverView.frame;
+    if (offsetY <= 264 - 64) {
+        originY = MAX(-offsetY + 264, -50 + 64);
+    }
+    hoverRect.origin.y = originY;
+    _hoverView.frame = hoverRect;
+}
+
+- (void)hxv_tableViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate {
+    // 跟随滑动
+    CGFloat offsetY = scrollView.contentOffset.y;
+    if (offsetY < _hoverView.bounds.size.height + _headerView.bounds.size.height) {
+        [self updateFollowContentOffsetWithScrollView:scrollView isGreaterThanTopHeader:YES];
     } else {
-        CGRect rect = _headerView.frame;
-        rect.origin.y =  MAX(-offsetY, - 200);
-        _headerView.frame = rect;
-        
-        CGRect hoverRect = _headerView.frame;
-        hoverRect.origin.y = 0;
-        _hoverView.frame = rect;
+        [self updateFollowContentOffsetWithScrollView:scrollView isGreaterThanTopHeader:NO];
+    }
+}
+
+- (void)hxv_tableViewDidEndDecelerating:(UIScrollView *)scrollView {
+    // 跟随滑动
+    CGFloat offsetY = scrollView.contentOffset.y;
+    if (offsetY < _hoverView.bounds.size.height + _headerView.bounds.size.height) {
+        [self updateFollowContentOffsetWithScrollView:scrollView isGreaterThanTopHeader:YES];
+    } else {
+        [self updateFollowContentOffsetWithScrollView:scrollView isGreaterThanTopHeader:NO];
     }
 }
 
 
-
-- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
-    NSLog(@"scrollViewDelegate:%@",NSStringFromCGPoint(scrollView.contentOffset));
-    
+- (void)updateFollowContentOffsetWithScrollView:(UIScrollView *)scrollView isGreaterThanTopHeader:(BOOL)yesOrNo {
+    CGPoint offset = yesOrNo ? scrollView.contentOffset : CGPointMake(0, _hoverView.bounds.size.height + _headerView.bounds.size.height);
+    NSMutableArray *ma = [@[@100, @101, @102, @103] mutableCopy];
+    if ([ma containsObject:@(scrollView.tag)]) {
+        [ma removeObject:@(scrollView.tag)];
+    }
+    for (NSNumber *tag in ma) {
+        UIView *view = [_scrollView viewWithTag:[tag integerValue]];
+        if ([view.subviews.firstObject isKindOfClass:[UIScrollView class]]) {
+            UIScrollView *tableView1 = (UIScrollView *)view.subviews.firstObject;
+            tableView1.contentOffset = offset;
+        }
+    }
 }
 
-- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate {
-    
-    
-}
-
-/** 滚动结束 */
-- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
-    
-    
-    
-}
 
 
 @end
